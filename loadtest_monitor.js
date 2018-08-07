01a8bef19917dafe53eff9efd9a28f140b1d3f8d
Sean Conklin <seanconklin@yahoo.com>
2018-08-07 13:24:27 -0700

    Added user account creation to make a more serious load, and removed fields from the payload CSV that were not critical

--- loadtest_monitor.js
+++ loadtest_monitor.js
@@ -1,17 +1,39 @@
 module.exports = {
-	setJSONBody: setJSONBody,
-	logHeaders: logHeaders
+	generateUsername,
+	setJSONBody,
+	logHeaders
 }
 
+// Flow Function To Generate Username
+function generateUsername( userContext, events, done ) {
+	const username = makeRandomStr( 10 );
+	userContext.vars.username = username;
+	return done();
+}
+
+// Helper Function For Randomization
+function makeRandomStr( size ) {
+	var text = '';
+	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for( var i = 1; i <= size; i ++ ) {
+		text += possible.charAt( Math.floor( Math.random() * possible.length ) );
+	}
+	return text;
+}
+
+// HTTP POST Before Function
 function setJSONBody( requestParams, context, ee, next ) {
 	return next();
 }
 
+// HTTP POST After Function
 function logHeaders( requestParams, response, context, ee, next ) {
 	if( response.body.error != undefined ) {
 		console.log( 'Error: ' + response.body.error );
 	} else if( response.body.id != undefined ) {
-		console.log( 'Created order ' + response.body.id );
+		console.log( 'Created customer or order record ID ' + response.body.id );
+	} else if( response.body != undefined ) {
+		console.log( response.body );
 	} else {
 		console.log( response );
 	}
