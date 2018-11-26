c8eedf164553dc98150d1b5f08e71ce4702e5939
Sean Conklin <sean@codedcommerce.com>
2018-11-25 21:59:02 -0800

    Updated README and added try-catch for better error handling

--- README.md
+++ README.md
@@ -16,8 +16,6 @@ First, install Node.JS onto your workstation, if not already installed.
 Next, install the artillery.io project:
 ```
 sudo npm install -g artillery --unsafe-perm=true --allow-root
-artillery -V
-artillery quick --count 10 -n 20 https://artillery.io/
 ```
 
 Or, to update an existing artillery.io installation:
@@ -25,7 +23,7 @@ Or, to update an existing artillery.io installation:
 sudo npm install -g artillery@latest --unsafe-perm=true --allow-root
 ```
 
-## Running the tests
+## Running the load test
 
 * Download this project to your workstation and change directory to it.
 * Create a read/write access ReST API account from WP Admin > WooCommerce > Settings > Advanced > REST API.
@@ -40,21 +38,20 @@ sudo npm install -g artillery@latest --unsafe-perm=true --allow-root
 
 ### Further Notes
 
-I recommend having an application performance monitoring tool running on your development environment so you can examine the bottlenecks that load testing exposes. The most popular tool is New Relic.
-
-The CSV file records are used randomly for load test source data. You can put as few or as many records in there you wish.
-
-Refer to the Artillery.io documentation for details on [configuration](https://artillery.io/docs/script-reference/).
-
-If you wish to place paid orders or do a more elaborate load test, for example using a credit card sandbox account, you will need to use more synthetic tools such as CasperJS to emulate a web browser.
+* I recommend having an application performance monitoring tool running on your development environment so you can examine the bottlenecks that load testing exposes. The most popular tool is New Relic.
+* The CSV file records are used randomly for load test source data. You can put as few or as many records in there you wish.
+* Refer to the Artillery.io documentation for details on [configuration](https://artillery.io/docs/script-reference/).
+* If you wish to place paid orders or do a more elaborate load test, for example using a credit card sandbox account, you will need to use more synthetic tools such as CasperJS to emulate a web browser.
 
 ### Synthetic Test
 
-The load test is not currently hooked into the synthetic test. Still working on that.
+* First, install Puppeteer by running  `sudo npm install -g puppeteer --unsafe-perm=true --allow-root`
+* Run Puppeteer (Chromium headless browser) with our test script `node synthetic-standalone.js`
 
-To install Puppeteer `sudo npm install -g puppeteer --unsafe-perm=true --allow-root`
+### Synthetic Load Test
 
-To run Puppeteer (Chromium headless browser) with our test script `node synthetic-test.js`
+* Run `artillery run loadtest_synthetic.yml -o results.json`.
+* Enter `artillery report results.json` to view a graphical report of results.
 
 ## Author
 
--- loadtest_synthetic.js
+++ loadtest_synthetic.js
@@ -1,4 +1,3 @@
-//'use strict';
 
 module.exports = { beforeRequest, afterResponse, runTest }
 
@@ -14,75 +13,85 @@ async function afterResponse( requestParams, response, context, ee, next ) {
 }
 
 async function runTest( context, events, next ) {
+	try {
 
-	// Open Browser
-	context.browser = await puppeteer.launch( { headless: true } );
-	context.page = await context.browser.newPage();
-	await context.page.setViewport( { width: 1280, height: 1280 } )
-	await context.page.goto( 'https://dev-sean-sandbox.pantheonsite.io/', {
-		timeout: 3000000
-	} );
+		// Open Browser
+		context.browser = await puppeteer.launch( { headless: true } );
+		context.page = await context.browser.newPage();
+		await context.page.setViewport( { width: 1280, height: 1280 } )
+		await context.page.goto( 'https://dev-sean-sandbox.pantheonsite.io/', {
+			timeout: 10000000
+		} );
 
-	// Click Add To Cart Button
-	await context.page.waitForSelector( "a[data-product_id='36']" );
-	//await context.page.screenshot( { path: 'step1-home.png', fullPage: true } );
-	await context.page.click( "a[data-product_id='36']" );
+		// Click Add To Cart Button
+		await context.page.waitForSelector( "a[data-product_id='36']" );
+		//await context.page.screenshot( { path: 'step1-home.png', fullPage: true } );
+		await context.page.click( "a[data-product_id='36']" );
 
-	// Click View Cart Button
-	await context.page.waitForSelector( 'a.added_to_cart' );
-	//await context.page.screenshot( { path: 'step2-added-to-cart.png', fullPage: true } );
-	await context.page.click( 'a.added_to_cart' );
+		// Click View Cart Button
+		await context.page.waitForSelector( 'a.added_to_cart' );
+		//await context.page.screenshot( { path: 'step2-added-to-cart.png', fullPage: true } );
+		await context.page.click( 'a.added_to_cart' );
 
-	// Click Checkout Button
-	await context.page.waitForSelector( 'a.checkout-button' );
-	//await context.page.screenshot( { path: 'step3-cart.png', fullPage: true } );
-	await context.page.click( 'a.checkout-button' );
+		// Click Checkout Button
+		await context.page.waitForSelector( 'a.checkout-button' );
+		//await context.page.screenshot( { path: 'step3-cart.png', fullPage: true } );
+		await context.page.click( 'a.checkout-button' );
 
-	// Checkout Page Loaded
-	await context.page.waitForSelector( 'button#place_order' );
+		// Checkout Page Loaded
+		await context.page.waitForSelector( 'button#place_order' );
 
-	// Complete Checkout Form
-	await context.page.type( 'input#billing_first_name', 'First Name' );
-	await context.page.type( 'input#billing_last_name', 'Last Name' );
-	await context.page.type( 'input#billing_address_1', '12345 Test St.' );
-	await context.page.type( 'input#billing_city', 'Los Angeles' );
-	await context.page.type( 'input#billing_postcode', '91307' );
-	await context.page.type( 'input#billing_phone', '111-222-3333' );
-	await context.page.type( 'input#billing_email', 'sean@codedcommerce.com' );
+		// Complete Checkout Form
+		await context.page.type( 'input#billing_first_name', 'First Name' );
+		await context.page.type( 'input#billing_last_name', 'Last Name' );
+		await context.page.type( 'input#billing_address_1', '12345 Test St.' );
+		await context.page.type( 'input#billing_city', 'Los Angeles' );
+		await context.page.type( 'input#billing_postcode', '91307' );
+		await context.page.type( 'input#billing_phone', '111-222-3333' );
+		await context.page.type( 'input#billing_email', 'sean@codedcommerce.com' );
 
-	// Complete Payment Method
-	await context.page.click( "label[for='payment_method_cod']" );
+		// Complete Payment Method
+		await context.page.click( "label[for='payment_method_cod']" );
 
-	// State Drop Down
-	await context.page.click( "label[for='billing_state']" );
-	await context.page.type( "label[for='billing_state']", 'California' + String.fromCharCode( 13 ) );
+		// State Drop Down
+		await context.page.click( "label[for='billing_state']" );
+		await context.page.type( "label[for='billing_state']", 'California' + String.fromCharCode( 13 ) );
 
-	// Wait For AJAX Refresh
-	await context.page.evaluate(() => {
-		let dom = document.querySelector( 'tr.order-total span.amount' );
-		dom.parentNode.removeChild( dom );
-	} );
-	await context.page.waitForSelector( 'tr.order-total span.amount' );
-	//var date = new Date();
-	//var timestamp = date.getTime();
-	//await context.page.screenshot( { path: 'step5-checkout-' + timestamp + '.png', fullPage: true } );
+		// Wait For AJAX Refresh
+		await context.page.evaluate(() => {
+			let dom = document.querySelector( 'tr.order-total span.amount' );
+			dom.parentNode.removeChild( dom );
+		} );
+		await context.page.waitForSelector( 'tr.order-total span.amount' );
 
-	// Submit Checkout
-	await context.page.click( 'button#place_order' );
+		// Checkout Screen Shot With Timestamp
+		//var date = new Date();
+		//var timestamp = date.getTime();
+		//await context.page.screenshot( { path: 'step5-checkout-' + timestamp + '.png', fullPage: true } );
 
-	// Order Receipt
-	await context.page.waitForSelector( 'li.order' );
-	const order_number = await context.page.evaluate(
-		() => document.querySelector( 'li.order strong' ).textContent
-	);
-	console.log( 'Order placed: ' + order_number.trim() );
-	//await context.page.screenshot( {
-	//	path: 'step6-order-' + order_number.trim() + '-placed.png',
-	//	fullPage: true
-	//} );
+		// Submit Checkout
+		await context.page.click( 'button#place_order' );
 
-	// Close Browser
-	await context.browser.close();
+		// Order Receipt
+		await context.page.waitForSelector( 'li.order' );
+		const order_number = await context.page.evaluate(
+			() => document.querySelector( 'li.order strong' ).textContent
+		);
+		console.log( 'Order placed: ' + order_number.trim() );
+
+		// Receipt Screen Shot With Order Number
+		//await context.page.screenshot( {
+		//	path: 'step6-order-' + order_number.trim() + '-placed.png',
+		//	fullPage: true
+		//} );
+
+		// Close Browser
+		await context.browser.close();
+
+	// Catch Errors
+	} catch( err ) {
+		console.log( err );
+	}
 
 	// Return
 	return next();
