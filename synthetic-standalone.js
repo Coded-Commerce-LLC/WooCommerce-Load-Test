86f7e1c8bb746285f2594f1104313b4b0de2f7ef
Sean Conklin <seanconklin@yahoo.com>
2018-11-26 13:19:38 -0800

    Re-enabled screenshots and added try/catch to standalone test

--- synthetic-standalone.js
+++ synthetic-standalone.js
@@ -2,71 +2,78 @@ const puppeteer = require( '/usr/local/lib/node_modules/puppeteer' );
 
 ( async function f() {
 
-	// Open Browser
-	const browser = await puppeteer.launch( { headless: true } );
-	const page = await browser.newPage();
-	await page.setViewport( { width: 1280, height: 1280 } )
-	await page.goto( 'https://dev-sean-sandbox.pantheonsite.io/' );
+	try {
 
-	// Click Add To Cart Button
-	await page.waitForSelector( "a[data-product_id='36']" );
-	//await page.screenshot( { path: 'step1-home.png', fullPage: true } );
-	await page.click( "a[data-product_id='36']" );
+		// Open Browser
+		const browser = await puppeteer.launch( { headless: true } );
+		const page = await browser.newPage();
+		await page.setViewport( { width: 1280, height: 1280 } )
+		await page.goto( 'https://dev-sean-sandbox.pantheonsite.io/' );
 
-	// Click View Cart Button
-	await page.waitForSelector( 'a.added_to_cart' );
-	//await page.screenshot( { path: 'step2-added-to-cart.png', fullPage: true } );
-	await page.click( 'a.added_to_cart' );
+		// Click Add To Cart Button
+		await page.waitForSelector( "a[data-product_id='36']" );
+		await page.screenshot( { path: 'step1-home.png', fullPage: true } );
+		await page.click( "a[data-product_id='36']" );
 
-	// Click Checkout Button
-	await page.waitForSelector( 'a.checkout-button' );
-	//await page.screenshot( { path: 'step3-cart.png', fullPage: true } );
-	await page.click( 'a.checkout-button' );
+		// Click View Cart Button
+		await page.waitForSelector( 'a.added_to_cart' );
+		await page.screenshot( { path: 'step2-added-to-cart.png', fullPage: true } );
+		await page.click( 'a.added_to_cart' );
 
-	// Checkout Page Loaded
-	await page.waitForSelector( 'button#place_order' );
+		// Click Checkout Button
+		await page.waitForSelector( 'a.checkout-button' );
+		await page.screenshot( { path: 'step3-cart.png', fullPage: true } );
+		await page.click( 'a.checkout-button' );
 
-	// Complete Checkout Form
-	await page.type( 'input#billing_first_name', 'First Name' );
-	await page.type( 'input#billing_last_name', 'Last Name' );
-	await page.type( 'input#billing_address_1', '12345 Test St.' );
-	await page.type( 'input#billing_city', 'Los Angeles' );
-	await page.type( 'input#billing_postcode', '91307' );
-	await page.type( 'input#billing_phone', '111-222-3333' );
-	await page.type( 'input#billing_email', 'sean@codedcommerce.com' );
+		// Checkout Page Loaded
+		await page.waitForSelector( 'button#place_order' );
 
-	// Complete Payment Method
-	await page.click( "label[for='payment_method_cod']" );
+		// Complete Checkout Form
+		await page.type( 'input#billing_first_name', 'First Name' );
+		await page.type( 'input#billing_last_name', 'Last Name' );
+		await page.type( 'input#billing_address_1', '12345 Test St.' );
+		await page.type( 'input#billing_city', 'Los Angeles' );
+		await page.type( 'input#billing_postcode', '91307' );
+		await page.type( 'input#billing_phone', '111-222-3333' );
+		await page.type( 'input#billing_email', 'sean@codedcommerce.com' );
 
-	// State Drop Down
-	await page.click( "label[for='billing_state']" );
-	await page.type( "label[for='billing_state']", 'California' + String.fromCharCode( 13 ) );
+		// Complete Payment Method
+		await page.click( "label[for='payment_method_cod']" );
 
-	// Wait For AJAX Refresh
-	await page.evaluate(() => {
-		let dom = document.querySelector( 'tr.order-total span.amount' );
-		dom.parentNode.removeChild( dom );
-	} );
-	await page.waitForSelector( 'tr.order-total span.amount' );
-	var date = new Date();
-	var timestamp = date.getTime();
-	await page.screenshot( { path: 'step5-checkout-' + timestamp + '.png', fullPage: true } );
+		// State Drop Down
+		await page.click( "label[for='billing_state']" );
+		await page.type( "label[for='billing_state']", 'California' + String.fromCharCode( 13 ) );
 
-	// Submit Checkout
-//	await page.click( 'button#place_order' );
+		// Wait For AJAX Refresh
+		await page.evaluate(() => {
+			let dom = document.querySelector( 'tr.order-total span.amount' );
+			dom.parentNode.removeChild( dom );
+		} );
+		await page.waitForSelector( 'tr.order-total span.amount' );
+		var date = new Date();
+		var timestamp = date.getTime();
+		await page.screenshot( { path: 'step5-checkout-' + timestamp + '.png', fullPage: true } );
 
-	// Order Receipt
-//	await page.waitForSelector( 'li.order' );
-//	const order_number = await page.evaluate(
-//		() => document.querySelector( 'li.order strong' ).textContent
-//	);
-//	console.log( 'Order placed: ' + order_number.trim() );
-	//await page.screenshot( {
-	//	path: 'step6-order-' + order_number.trim() + '-placed.png',
-	//	fullPage: true
-	//} );
+		// Submit Checkout
+		await page.click( 'button#place_order' );
 
-	// End
-	await browser.close();
+		// Order Receipt
+		await page.waitForSelector( 'li.order' );
+		const order_number = await page.evaluate(
+			() => document.querySelector( 'li.order strong' ).textContent
+		);
+		console.log( 'Order placed: ' + order_number.trim() );
+		await page.screenshot( {
+			path: 'step6-order-' + order_number.trim() + '-placed.png',
+			fullPage: true
+		} );
+
+		// End
+		await browser.close();
+
+	// Catch Errors
+	} catch( err ) {
+		console.log( err );
+	}
 
 } )();
